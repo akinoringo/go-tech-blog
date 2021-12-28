0dc1be1eabe89c4958c786ae47d19380b9c16442
akinoringo <wbraver@outlook.jp>
2021-12-28 10:49:54 +0900

    データベースから値を取得して表示

---
+++ handler/render.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/flosch/pongo2"
+	"github.com/labstack/echo/v4"
+)
+
+const tmplPath = "src/template/"
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	b, error := htmlBlob(file, data)
+	if error != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.HTMLBlob(http.StatusOK, b)
+}
--- main.go
+++ main.go
@@ -2,30 +2,28 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
-	"strconv"
-	"time"
 
-	"github.com/flosch/pongo2"
+	"go-tech-blog/handler"
+	"go-tech-blog/repository"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-const tmplPath = "src/template/"
-
 var db *sqlx.DB
 var e = createMux()
 
 func main() {
 	db = connectDB()
+	repository.SetDB(db)
 
-	e.GET("/", articleIndex)
-	e.GET("/new", articleNew)
-	e.GET("/:id", articleShow)
-	e.GET("/:id/edit", articleEdit)
+	e.GET("/", handler.ArticleIndex)
+	e.GET("/new", handler.ArticleNew)
+	e.GET("/:id", handler.ArticleShow)
+	e.GET("/:id/edit", handler.ArticleEdit)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
@@ -54,58 +52,4 @@ func createMux() *echo.Echo {
 	e.Static("/js", "src/js")
 
 	return e
-}
-
-func articleIndex(c echo.Context) error {
-	// return c.String(http.StatusOK, "Hello, World!!")
-	data := map[string]interface{}{
-		"Message": "Article Index",
-		"Now":     time.Now(),
-	}
-
-	return render(c, "article/index.html", data)
-}
-
-func articleNew(c echo.Context) error {
-	data := map[string]interface{}{
-		"Message": "Article New",
-		"Now":     time.Now(),
-	}
-	return render(c, "article/new.html", data)
-}
-
-func articleShow(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-
-	data := map[string]interface{}{
-		"Message": "Article Show",
-		"Now":     time.Now(),
-		"ID":      id,
-	}
-
-	return render(c, "article/show.html", data)
-}
-
-func articleEdit(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-
-	data := map[string]interface{}{
-		"Message": "Article Edit",
-		"Now":     time.Now(),
-		"ID":      id,
-	}
-
-	return render(c, "article/edit.html", data)
-}
-
-func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
-	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
-}
-
-func render(c echo.Context, file string, data map[string]interface{}) error {
-	b, error := htmlBlob(file, data)
-	if error != nil {
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	return c.HTMLBlob(http.StatusOK, b)
 }
